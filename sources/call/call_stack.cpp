e31ee14132ca0ea6fdccc72418a260c0cb523c05
qua <qualab@gmail.com>
2016-04-21 00:29:24 +0300

    Renamed call::stack::const_iterator to simply call::stack::iterator.

--- sources/call/call_stack.cpp
+++ sources/call/call_stack.cpp
@@ -31,14 +31,14 @@ namespace trace
         return tl_calls.size();
     }
 
-    call::stack::const_iterator call::stack::begin()
+    call::stack::iterator call::stack::begin()
     {
-        return call::stack::const_iterator(call::stack::const_iterator::data(tl_calls.begin()));
+        return call::stack::iterator(call::stack::iterator::data(tl_calls.begin()));
     }
 
-    call::stack::const_iterator call::stack::end()
+    call::stack::iterator call::stack::end()
     {
-        return call::stack::const_iterator(call::stack::const_iterator::data(tl_calls.end()));
+        return call::stack::iterator(call::stack::iterator::data(tl_calls.end()));
     }
 
     call::warden::warden()
@@ -60,92 +60,92 @@ namespace trace
         }
     }
 
-    call::stack::const_iterator::const_iterator(call::stack::const_iterator::data&& new_data)
-        : m_data(new(m_storage) call::stack::const_iterator::data(std::move(new_data)))
+    call::stack::iterator::iterator(call::stack::iterator::data&& new_data)
+        : m_data(new(m_storage) call::stack::iterator::data(std::move(new_data)))
     {
     }
 
-    call::stack::const_iterator::const_iterator(const call::stack::const_iterator& another)
-        : m_data(new(m_storage) call::stack::const_iterator::data(*another.m_data))
+    call::stack::iterator::iterator(const call::stack::iterator& another)
+        : m_data(new(m_storage) call::stack::iterator::data(*another.m_data))
     {
     }
 
-    call::stack::const_iterator::const_iterator(call::stack::const_iterator&& temporary)
-        : m_data(new(m_storage) call::stack::const_iterator::data(std::move(*temporary.m_data)))
+    call::stack::iterator::iterator(call::stack::iterator&& temporary)
+        : m_data(new(m_storage) call::stack::iterator::data(std::move(*temporary.m_data)))
     {
     }
 
-    call::stack::const_iterator::~const_iterator()
+    call::stack::iterator::~iterator()
     {
         m_data->~data();
     }
 
-    call::stack::const_iterator& call::stack::const_iterator::operator ++ ()
+    call::stack::iterator& call::stack::iterator::operator ++ ()
     {
         ++m_data->get_reference();
         return *this;
     }
 
-    call::stack::const_iterator& call::stack::const_iterator::operator -- ()
+    call::stack::iterator& call::stack::iterator::operator -- ()
     {
         --m_data->get_reference();
         return *this;
     }
 
-    call::stack::const_iterator call::stack::const_iterator::operator ++ (int)
+    call::stack::iterator call::stack::iterator::operator ++ (int)
     {
-        const_iterator result = *this;
+        iterator result = *this;
         ++m_data->get_reference();
         return result;
     }
 
-    call::stack::const_iterator call::stack::const_iterator::operator -- (int)
+    call::stack::iterator call::stack::iterator::operator -- (int)
     {
-        const_iterator result = *this;
+        iterator result = *this;
         --m_data->get_reference();
         return result;
     }
 
-    call::stack::const_iterator call::stack::const_iterator::operator + (size_t delta) const
+    call::stack::iterator call::stack::iterator::operator + (size_t delta) const
     {
-        const_iterator result = *this;
+        iterator result = *this;
         return result += delta;
     }
 
-    call::stack::const_iterator call::stack::const_iterator::operator - (size_t delta) const
+    call::stack::iterator call::stack::iterator::operator - (size_t delta) const
     {
-        const_iterator result = *this;
+        iterator result = *this;
         return result -= delta;
     }
 
-    size_t call::stack::const_iterator::operator - (const call::stack::const_iterator& another) const
+    size_t call::stack::iterator::operator - (const call::stack::iterator& another) const
     {
         return m_data->get_value() - another.m_data->get_value();
     }
 
-    call::stack::const_iterator& call::stack::const_iterator::operator += (size_t delta)
+    call::stack::iterator& call::stack::iterator::operator += (size_t delta)
     {
         m_data->get_reference() += delta;
         return *this;
     }
 
-    call::stack::const_iterator& call::stack::const_iterator::operator -= (size_t delta)
+    call::stack::iterator& call::stack::iterator::operator -= (size_t delta)
     {
         m_data->get_reference() -= delta;
         return *this;
     }
 
-    call::stack::const_iterator::data::data(call::stack::const_iterator::data::value_type value)
+    call::stack::iterator::data::data(call::stack::iterator::data::value_type value)
         : m_value(value)
     {
     }
 
-    call::stack::const_iterator::data::value_type call::stack::const_iterator::data::get_value() const
+    call::stack::iterator::data::value_type call::stack::iterator::data::get_value() const
     {
         return m_value;
     }
 
-    call::stack::const_iterator::data::value_type& call::stack::const_iterator::data::get_reference()
+    call::stack::iterator::data::value_type& call::stack::iterator::data::get_reference()
     {
         return m_value;
     }
